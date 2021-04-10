e75c188ee88d800f3953d5ad2d59dff7db8832b2
Junbeom Lee <junbeoml22@gmail.com>
2021-04-10 22:28:08 +0900

    surfacetesting

--- fitting.py
+++ fitting.py
@@ -111,25 +111,26 @@ class SurfaceFit:
     def __init__(self,
                  calc_date, dates, 
                  strikes, volData,
+                 fitter,
                  weight = True,
                  weight_cut = 0.6):
         diff = np.array(dates) - calc_date
         self.times = np.array([x.days/365.0 for x in diff])
-        self.slice_num = length(self.times)
+        self.slice_num = len(self.times)
         self.volData = np.array(volData) * 0.01
         self.totalVar = np.power(self.volData, 2.0) * self.times.reshape(-1, 1)
         self.logStrikes = np.log(strikes)
-        self.fitter = [ssvi.Ssvi([-0.3, 0.01], phi.QuotientPhi([0.4, 0.4])) for i in range(self.slice_num)]
-        self.vectorized_fitter = [np.vectorize(fitter[i]) for i in i in range(self.slice_num)]
+        self.fitter = fitter
+        self.vectorized_fitter = [np.vectorize(fitter[i]) for i in range(self.slice_num)]
         self.butterfly = ssvi.ssviQuotientConstraints
         if weight:
             self.weight = 5.0 * np.maximum(0.0, np.abs(np.log(weight_cut)) - np.abs(self.logStrikes))
         else:
-            self.weight = np.repeat(1.0, length(self.logStrikes))
+            self.weight = np.repeat(1.0, len(self.logStrikes))
         self.params = [None for i in range(self.slice_num)]
         self.calendar_checker = np.linspace(-1.0, 1.0, 41)
         
-    def cost_function(self, x, i):
+    def cost_function(self, i, x):
         """
         cost_function(self, x)
         rho, theta, eta, gamma = x
@@ -148,15 +149,13 @@ class SurfaceFit:
         _cost_function = partial(self.cost_function, i)
         if i == self.slice_num - 1:
             res = minimize(
-                self.cost_function, _init,
+                _cost_function, _init,
                 constraints = self.butterfly,
                 method = method,
                 options = {'disp': verbose, 'maxiter': maxiter},
                 tol = tol
             )
         else:
-            #current_fitter = copy.deepcopy(self.vectorized_fitter[i])
-            #upper_fitter = copy.deepcopy(self.vectorized_fitter[i+1])
             _cost_function = partial(self.cost_function, i)
             def calendar(x):
                 # rho, theta, eta, gamma
@@ -168,12 +167,14 @@ class SurfaceFit:
                 _fitter.reset(x)
                 vf = np.vectorize(_fitter)
                 ret = self.vectorized_fitter[i+1](self.calendar_checker) - constant.eps -vf(self.calendar_checker)
-            calendar_cons = ({'type': 'ineq',
+            cons = ({'type': 'ineq',
                      'fun': calendar}
-                    )
+                    ) # calendar
+            for c in self.butterfly:
+                cons.update(c)
             res = minimize(
-                self.cost_function, _init,
-                constraints = [self.butterfly, calendar_cons],
+                _cost_function, _init,
+                constraints = cons,
                 method = method,
                 options = {'disp': verbose, 'maxiter': maxiter},
                 tol = tol
@@ -196,7 +197,7 @@ class SurfaceFit:
         volatility point at k
         Note: k is log strike
         """
-        return np.sqrt(self.vectorized_fitter[i](k) /self.t  )
+        return np.sqrt(self.vectorized_fitter[i](k) /self.times[i]  )
 
     def fitted_slice(self, i):
         """
@@ -204,24 +205,29 @@ class SurfaceFit:
         volatility point at k
         Note: k is log strike
         """
-        return np.sqrt( self.vectorized_fitter[i](self.logStrikes) / self.t )
+        import pdb;pdb.set_trace()
+        return np.sqrt( self.vectorized_fitter[i](self.logStrikes) / self.times[i] )
 
-    def visualize_slice(self):
+    def visualize(self):
         st = np.exp(self.logStrikes)
         lst = self.logStrikes
         ax_size = int(self.slice_num/2.0)+1
         fig, axs = plt.subplots(ax_size, 2)
         count = 0
-        
-        for i in range(ax_size):
-            axs[i, 0].plot(st, self.fitted_slice(count), 'r--')
-            axs[i, 0].plot(st, self.volData[count]), 'b^')
-            axs[i, 0].set_title("T ="+"{:10.2f}".format(self.times[count]))
-            count += 1
-            axs[i, 1].plot(st, self.fitted_slice(count), 'r--')
-            axs[i, 1].plot(st, self.volData[count]), 'b^')
-            axs[i, 1].set_title("T ="+"{:10.2f}".format(self.times[count]))
-            count +=1
+        if ax_size == 1:
+            axs[0, 0].plot(st, self.fitted_slice(count), 'r--')
+            axs[0, 0].plot(st, self.volData[count], 'b^')
+            axs[0, 0].set_title("T ="+"{:10.2f}".format(self.times[count]))
+        else:
+            for i in range(ax_size):
+                axs[i, 0].plot(st, self.fitted_slice(count), 'r--')
+                axs[i, 0].plot(st, self.volData[count], 'b^')
+                axs[i, 0].set_title("T ="+"{:10.2f}".format(self.times[count]))
+                count += 1
+                axs[i, 1].plot(st, self.fitted_slice(count), 'r--')
+                axs[i, 1].plot(st, self.volData[count], 'b^')
+                axs[i, 1].set_title("T ="+"{:10.2f}".format(self.times[count]))
+                count +=1
             
         plt.legend()
         plt.show()   
