e8c6812d311a054b33030fdb358bcae4f78d9ba5
Junbeom Lee <junbeoml22@gmail.com>
2024-05-13 13:24:30 +0900

    some minor changes and the final msg is changed

--- msg_box.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import messagebox
-
-def show_message_box(msg, title):
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window
-
-    messagebox.showinfo(title, msg)
-
